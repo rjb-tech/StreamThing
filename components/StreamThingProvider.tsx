2d7866d9bf2b1c7e69cf2290ca42ec07b392e0b3
Ryne Burden <ryneburden@protonmail.com>
2022-12-22 02:00:17 -0500

    setup

--- components/StreamThingProvider.tsx
+++ components/StreamThingProvider.tsx
@@ -1,3 +1,4 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { ReactNode } from "react";
 
 interface ProviderProps {
@@ -5,5 +6,20 @@ interface ProviderProps {
 }
 
 export const StreamThingProvider = ({ children }: ProviderProps) => {
-  return <div>{children}</div>;
+  const { user, isLoading, isAuthenticated, loginWithRedirect } = useAuth0();
+  return (
+    <div className="bg-emerald-500 h-screen w-screen">
+      <header className="container h-24 w-full p-4 bg-yellow-100 flex items-center justify-between">
+        <button
+          id="network-info-dropdown"
+          className="relative bg-pink-300 h-full w-16 rounded-full"
+        ></button>
+        <button
+          id="userInfoDropdown"
+          className="h-full w-32 bg-blue-300 rounded-md"
+        ></button>
+      </header>
+      {children}
+    </div>
+  );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { StreamThingProvider } from "../components/StreamThingProvider";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <StreamThingProvider>
-      <Component {...pageProps} />
-    </StreamThingProvider>
+    <Auth0Provider
+      domain="stream-thing.us.auth0.com"
+      clientId="cSOsosd0EQmMNRPHC7fZe2hwS1RL39NV"
+      redirectUri="localhost:3000"
+    >
+      <StreamThingProvider>
+        <Component {...pageProps} />
+      </StreamThingProvider>
+    </Auth0Provider>
   );
 }
