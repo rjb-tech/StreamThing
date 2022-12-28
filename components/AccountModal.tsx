6cd7aa456df6c37cab73cf3317f98902bc1c10a2
Ryne Burden <ryneburden@protonmail.com>
2022-12-27 23:00:40 -0500

    remove comment

--- components/AccountModal.tsx
+++ components/AccountModal.tsx
@@ -50,17 +50,6 @@ export const AccountModal = () => {
     <>
       <Transition as="div" show={showAccountModal}>
         <Dialog as="div" className="relative z-10" onClose={closeModal}>
-          {/* <Transition.Child
-            as="span"
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black bg-opacity-25 z-10" />
-          </Transition.Child> */}
           <div className="fixed inset-0 overflow-y-auto">
             <div className="flex min-h-full items-center justify-center p-4 text-center">
               <Transition.Child
