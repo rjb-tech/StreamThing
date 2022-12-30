e00c4b14046b052a605a04c4bc12e32d786d9454
Ryne Burden <ryneburden@protonmail.com>
2022-12-30 01:20:36 -0500

    edit button

--- components/UnauthenticatedHeader.tsx
+++ components/UnauthenticatedHeader.tsx
@@ -1,6 +1,7 @@
 import { ArrowLeftOnRectangleIcon } from "@heroicons/react/20/solid";
 import { useAppDispatch } from "../redux/hooks";
 import { setShowAuthModal } from "../redux/slices/mainSlice";
+import { StreamThingButton } from "./StreamThingButton";
 
 interface UnauthenticatedHeaderProps {
   supabaseClient: any;
@@ -15,13 +16,12 @@ export const UnauthenticatedHeader = ({
       <div className="p-4 w-full h-full rounded-lg flex items-center justify-between">
         <span className="text-4xl text-white">StreamThing</span>
         <span className="w-28 flex justify-between">
-          <button
-            onClick={() => dispatch(setShowAuthModal(true))}
-            className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+          <StreamThingButton
+            innerText="Login"
+            clickFn={() => dispatch(setShowAuthModal(true))}
           >
-            <span className="pr-2">Login</span>
-            <ArrowLeftOnRectangleIcon className="w-6 h-6" />
-          </button>
+            <ArrowLeftOnRectangleIcon className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100" />
+          </StreamThingButton>
         </span>
       </div>
     </header>
