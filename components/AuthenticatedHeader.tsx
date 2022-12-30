7e4cdbcfdafcc7b3f34aa163424f948f87a9073c
Ryne Burden <ryneburden@protonmail.com>
2022-12-29 22:28:49 -0500

    add network name as text on network button

--- components/AuthenticatedHeader.tsx
+++ components/AuthenticatedHeader.tsx
@@ -30,7 +30,7 @@ export const AuthenticatedHeader = ({
   const { username, fullName, activeNetwork } = useAppSelector(
     (state) => state.account
   );
-  const { logoUrl } = useAppSelector((state) => state.network);
+  const { logoUrl, name } = useAppSelector((state) => state.network);
 
   return (
     <header className="fixed top-0 h-24 w-full py-4 px-8 bg-gradient-to-r from-[#7180B9] to-[#171738] flex items-center justify-between z-50">
@@ -68,7 +68,7 @@ export const AuthenticatedHeader = ({
             onClick={() => dispatch(setShowGuide(!showGuide))}
             className="guide-button inline-flex w-fit h-1/2 justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
           >
-            Network
+            {name}
             <TvIcon
               className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
               aria-hidden="true"
