01b4f6660429901fc7b5c291dab7a95d96c8fcc6
Ryne Burden <ryneburden@protonmail.com>
2022-12-30 00:50:55 -0500

    edit network menu header thing

--- components/ChannelGuide.tsx
+++ components/ChannelGuide.tsx
@@ -1,35 +1,23 @@
 import { ChannelRow } from "./ChannelRow";
 import Image from "next/image";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
-import { StreamThingButton } from "./StreamThingButton";
-import { setShowEditMembersModal } from "../redux/slices/mainSlice";
 
 export const ChannelGuide = () => {
   const dispatch = useAppDispatch();
-  const { logoUrl } = useAppSelector((state) => state.network);
-  const { networks } = useAppSelector((state) => state.account);
+  const { logoUrl, name } = useAppSelector((state) => state.network);
   return (
     <div className="absolute pt-24 h-fit w-full bg-gray-500">
-      <div className="network-header p-3 w-full flex items-center justify-between">
+      <div className="network-header py-3 px-6 w-full flex items-center justify-between">
         <div className="w-fit flex space-x-4">
-          {networks?.map((network) => {
-            return (
-              <Image
-                className="rounded-full h-20 w-20 bg-[#D6E5E3] ring-2 ring-white ring-opacity-40 text-sm font-medium opacity-100"
-                src={logoUrl || ""}
-                alt="network image"
-                height={100}
-                width={100}
-              />
-            );
-          })}
-        </div>
-        <div className="w-fit flex justify-between space-x-4">
-          <StreamThingButton
-            innerText="Edit Members"
-            clickFn={() => dispatch(setShowEditMembersModal(true))}
+          <Image
+            className="rounded-full h-20 w-20 bg-[#D6E5E3] ring-2 ring-white ring-opacity-40 text-sm font-medium opacity-100"
+            src={logoUrl || ""}
+            alt="network image"
+            height={100}
+            width={100}
           />
         </div>
+        <span className="text-3xl text-white">{name}</span>
       </div>
       <ChannelRow streamUrl="https://www.twitch.tv/pobelter" user="Ryne" />
       <ChannelRow
