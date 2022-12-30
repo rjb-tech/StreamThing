11cbec0655b5c43cd8e6d367dc03cd3a29a87f67
Ryne Burden <ryneburden@protonmail.com>
2022-12-30 02:26:38 -0500

    add owner buttons to network menu

--- components/ChannelRow.tsx
+++ components/ChannelRow.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
-import { useAppDispatch } from "../redux/hooks";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { setActiveStream, setShowGuide } from "../redux/slices/mainSlice";
 import type { NetworkMember } from "../redux/slices/networkSlice";
+import { ChevronDoubleUpIcon, UserMinusIcon } from "@heroicons/react/20/solid";
 
 interface ChannelProps {
   streamUrl: string;
@@ -10,6 +11,7 @@ interface ChannelProps {
 
 export const ChannelRow = ({ streamUrl, user }: ChannelProps) => {
   const dispatch = useAppDispatch();
+  const { owner } = useAppSelector((state) => state.network);
   const handleStreamClick = () => {
     dispatch(setActiveStream(streamUrl));
     dispatch(setShowGuide(false));
@@ -17,7 +19,7 @@ export const ChannelRow = ({ streamUrl, user }: ChannelProps) => {
   return (
     <div className="flex overscroll-y-contain overflow-y-scroll border-t-4">
       <section className="bg-black h-full w-1/5 flex justify-center">
-        <div className="bg-emerald-200 h-20 w-full lg:flex items-center justify-around mx-3">
+        <div className="bg-emerald-200 h-20 w-full lg:flex items-center justify-between lg:px-6">
           <Image
             className="rounded-full h-12 w-12"
             src={user.avatarUrl}
@@ -25,7 +27,16 @@ export const ChannelRow = ({ streamUrl, user }: ChannelProps) => {
             width={100}
             alt={`${user.fullName} avatar`}
           />
-          {user.username || user.fullName}
+
+          <div className="w-fit flex flex-col items-center justify-center">
+            {user.username || user.fullName}
+            {user.id === owner && (
+              <span className="flex w-fit flex space-x-4 pt-1">
+                <ChevronDoubleUpIcon className="h-5 w-5" />
+                <UserMinusIcon className="h-5 w-5" />
+              </span>
+            )}
+          </div>
         </div>
       </section>
       <section className="bg-red-200 h-full w-full">
