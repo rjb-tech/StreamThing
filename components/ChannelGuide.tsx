11f21543c0cbcfacd9fb582696585ddb3c166794
Ryne Burden <ryneburden@protonmail.com>
2022-12-31 00:48:09 -0500

    add row for authenticated user

--- components/ChannelGuide.tsx
+++ components/ChannelGuide.tsx
@@ -1,12 +1,27 @@
 import { ChannelRow } from "./ChannelRow";
-import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import { useAppSelector } from "../redux/hooks";
 import type { FriendRecord } from "./types";
+import { useUser } from "@supabase/auth-helpers-react";
 
 export const ChannelGuide = () => {
-  const { friends } = useAppSelector((state) => state.account);
+  const user = useUser();
+  const { friends, username, fullName, avatarUrl } = useAppSelector(
+    (state) => state.account
+  );
+
+  const userRecord: FriendRecord = {
+    id: user?.id || "",
+    username: username || "",
+    fullName: fullName || "",
+    avatarUrl: avatarUrl || "",
+  };
 
   return (
     <div className="absolute pt-24 h-fit w-full bg-gray-500">
+      <ChannelRow
+        streamUrl="https://www.twitch.tv/pobelter"
+        user={userRecord}
+      />
       {friends?.map((member: FriendRecord) => {
         return (
           <ChannelRow
