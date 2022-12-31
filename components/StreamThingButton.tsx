4996bb84a3b0f1d696fcd0b5a825b5c081d066d0
Ryne Burden <ryneburden@protonmail.com>
2022-12-30 19:13:56 -0500

    change disabled state change duration

--- components/StreamThingButton.tsx
+++ components/StreamThingButton.tsx
@@ -22,7 +22,7 @@ export const StreamThingButton = ({
       onClick={clickFn}
       type={buttonType}
       className={classNames(
-        "inline-flex w-fit h-1/2 justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white transition-all duration-250",
+        "inline-flex w-fit h-1/2 justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white transition-all duration-300",
         {
           "hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75":
             !disabled,
