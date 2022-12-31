7b4628aa878e0372af8119aa1f2e97ebc4c0b3be
Ryne Burden <ryneburden@protonmail.com>
2022-12-31 00:02:32 -0500

    remove any and all network modals

--- components/ModalProvider.tsx
+++ components/ModalProvider.tsx
@@ -1,15 +1,11 @@
 import { AccountModal } from "./AccountModal";
 import { AuthModal } from "./AuthModal";
-import { CreateNetworkModal } from "./CreateNetworkModal";
-import { EditMembersModal } from "./EditMembersModal";
 
 export const ModalProvider = () => {
   return (
     <>
       <AuthModal />
       <AccountModal />
-      <CreateNetworkModal />
-      <EditMembersModal />
     </>
   );
 };
