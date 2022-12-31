c52f9847f26a41fd84b35f0d5ea75dc9fac6f0d3
Ryne Burden <ryneburden@protonmail.com>
2022-12-30 19:12:59 -0500

    add disabled state to button component

--- components/StreamThingButton.tsx
+++ components/StreamThingButton.tsx
@@ -1,10 +1,12 @@
 import { ReactNode } from "react";
+import classNames from "classnames";
 
 interface StreamThingButtonProps {
   innerText: string | undefined;
   children?: ReactNode; // This is intended for icons from hero icons
   clickFn?(): void;
   buttonType?: "button" | "submit" | "reset" | undefined;
+  disabled?: boolean;
 }
 
 export const StreamThingButton = ({
@@ -12,12 +14,21 @@ export const StreamThingButton = ({
   clickFn,
   children,
   buttonType = undefined,
+  disabled = false,
 }: StreamThingButtonProps) => {
   return (
     <button
+      disabled={disabled}
       onClick={clickFn}
       type={buttonType}
-      className="inline-flex w-fit h-1/2 justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+      className={classNames(
+        "inline-flex w-fit h-1/2 justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white transition-all duration-250",
+        {
+          "hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75":
+            !disabled,
+          "bg-opacity-10 text-gray-800": disabled,
+        }
+      )}
     >
       {innerText}
       {children}
