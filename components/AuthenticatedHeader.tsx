2494478755420129f93f161d19c98a07d911c9df
Ryne Burden <ryneburden@protonmail.com>
2022-12-30 19:56:06 -0500

    change copy

--- components/AuthenticatedHeader.tsx
+++ components/AuthenticatedHeader.tsx
@@ -38,7 +38,7 @@ export const AuthenticatedHeader = ({
         {activeNetwork && (
           <StreamThingButton
             clickFn={() => dispatch(setShowGuide(!showGuide))}
-            innerText="Network Menu"
+            innerText="Network"
           >
             <TvIcon
               className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
