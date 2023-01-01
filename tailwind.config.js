56b9f1a8993dfd9bbe9a43aed904cf1a57d3f731
Ryne Burden <ryneburden@protonmail.com>
2023-01-01 01:56:32 -0500

    slow down animation a bit more

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     extend: {
       animation: {
         "ping-5": "ping 1.5s cubic-bezier(0, 0, 0.2, 1) 5",
-        "ping-slow": "ping 1.5s cubic-bezier(0, 0, 0.2, 1) infinite",
+        "ping-slow": "ping 2s cubic-bezier(0, 0, 0.2, 1) infinite",
       },
     },
   },
